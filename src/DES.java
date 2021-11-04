8f14e702288366c5405ebd2bab181630730b2db4
benshanfuhai <2628254294@qq.com>
2021-11-04 17:45:36 +0800

    update des.

--- src/DES.java
+++ src/DES.java
@@ -1,82 +1,40 @@
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
-import java.security.SecureRandom;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class DES {
     public static void main(String[] args) {
+        // 明文
         String message = "Hello World!";
+        // 秘钥，使用DES加密时，必须为8位
         String key = "12345678";
-        System.out.println("明文：" + message);
-        String ciphertext = encrypt(message, key);
-        System.out.println("密文：" + ciphertext);
-        System.out.println("明文：" + decrypt(ciphertext, key));
-
+        String encryptText = encrypt(message, key);
+        System.out.println("密文：" + encryptText);
+        String decryptText = decrypt(encryptText, key);
+        System.out.println("明文：" + decryptText);
     }
 
     /**
-     * 基本数据加密
+     * 加密
      *
      * @param message 明文
      * @param key     秘钥
-     * @return 加密后的结果
+     * @return 密文
      */
     public static String encrypt(String message, String key) {
-        if (message.isEmpty() || key.isEmpty()) {
-            return null;
-        }
         try {
-            byte[] byteMessage = encrypt(message.getBytes(), key.getBytes());
-            return new BASE64Encoder().encode(byteMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * 基于字节加密
-     *
-     * @param message 明文
-     * @param key     秘钥
-     * @return 加密后的结果
-     */
-    public static byte[] encrypt(byte[] message, byte[] key) throws Exception {
-        // 产生可信任的随机数源
-        SecureRandom secureRandom = new SecureRandom();
-        // 基于密钥数据创建DESKeySpec对象
-        DESKeySpec desKeySpec = new DESKeySpec(key);
-        // 创建密钥工厂，将DESKeySpec转换成SecretKey对象来保存对称密钥
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
-        SecretKey secretKey = secretKeyFactory.generateSecret(desKeySpec);
-        // Cipher实际完成加密操作，指定其加密算法
-        Cipher cipher = Cipher.getInstance("DES");
-        // 初始化Cipher对象，ENCRYPT_MODE表示加密
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, secureRandom);
-        // 加密
-        return cipher.doFinal(message);
-    }
-
-    /**
-     * 基本数据解密
-     *
-     * @param ciphertext 密文
-     * @param key        秘钥
-     * @return 解密后的结果
-     */
-    public static String decrypt(String ciphertext, String key) {
-        if (ciphertext.isEmpty() || key.isEmpty()) {
-            return null;
-        }
-        try {
-            BASE64Decoder base64Decoder = new BASE64Decoder();
-            byte[] bufCiphertext = base64Decoder.decodeBuffer(ciphertext);
-            byte[] message = decrypt(bufCiphertext, key.getBytes());
-            return new String(message);
+            // 创建加密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            // 创建加密规则，第一个参数表示key的字节码，第二个参数表示加密类型
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "DES");
+            // 进行加密初始化，第一个参数表示加密模式，第二个参数表示加密规则
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            // 调用加密方法，参数表示明文的字节码
+            byte[] cipherBytes = cipher.doFinal(message.getBytes());
+            // 创建Base64对象，注意导入Apache的包
+            return Base64.encode(cipherBytes);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -85,26 +43,26 @@ public class DES {
 
 
     /**
-     * 基于字节解密
+     * 解密
      *
-     * @param ciphertext 密文
-     * @param key        秘钥
-     * @return 加密后的结果
-     * @throws Exception 各种异常
+     * @param encryptText 密文
+     * @param key         秘钥
+     * @return 明文
      */
-    public static byte[] decrypt(byte[] ciphertext, byte[] key) throws Exception {
-        // 产生可信任的随机数源
-        SecureRandom secureRandom = new SecureRandom();
-        // 基于密钥数据创建DESKeySpec对象
-        DESKeySpec desKeySpec = new DESKeySpec(key);
-        // 创建密钥工厂，将DESKeySpec转换成SecretKey对象来保存对称密钥
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
-        SecretKey secretKey = secretKeyFactory.generateSecret(desKeySpec);
-        // Cipher实际完成加密操作，指定其解密算法
-        Cipher cipher = Cipher.getInstance("DES");
-        // 初始化Cipher对象，DECRYPT_MODE表示解密
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, secureRandom);
-        // 解密
-        return cipher.doFinal(ciphertext);
+    public static String decrypt(String encryptText, String key) {
+        try {
+            // 创建解密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            // 创建解密规则，第一个参数表示秘钥的字节码，第二个参数表示解密类型
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "DES");
+            // 进行解密初始化，第一个参数表示解密模式，第二个参数表示解密规则
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+            // 调用解密方法，参数表示密文的字节码
+            byte[] decryptBytes = cipher.doFinal(Base64.decode(encryptText));
+            return new String(decryptBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
